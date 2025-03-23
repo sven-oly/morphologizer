e29babe6dd47227531ebd252c0d9c4bb97474152
Craig Cornelius <cwcornelius@gmail.com>
2025-03-23 16:51:35 -0700

    Add morphy_base

---
+++ morphy_base.py
@@ -0,0 +1,14 @@
+# Base class for morphologizer
+
+class morphy_base():
+    def __init__(self):
+        self.lang_name = 'unknown'
+        self.lang_code = 'und'
+        self.grammar = None
+        sself.fsts = None
+
+    def generate(self):
+        return None
+
+    def parse(self):
+        return None
